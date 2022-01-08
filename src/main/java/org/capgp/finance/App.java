d1ab8155485dc7db236816a8d071c494f49e4539
rahman <rahmanmohammed2510@gmail.com>
2022-01-08 16:36:32 +0000

    commit changes

---
+++ src/main/java/org/capgp/finance/App.java
@@ -0,0 +1,13 @@
+package org.capgp.finance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
